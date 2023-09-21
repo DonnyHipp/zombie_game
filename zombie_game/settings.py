9d726af50b97c861645d64baf719059428e7c401
DonnyHipp <toloknov.igor123@gmail.com>
2023-09-21 15:50:32 +0300

    start project

---
+++ zombie_game/settings.py
@@ -0,0 +1,16 @@
+SCREEN_TITLE = "Zombie game"
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+# PLAYER_JUMP_SPEED = 20
